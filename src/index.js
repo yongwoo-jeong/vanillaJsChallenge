1fa5a39f55ac2de5f97fbae6986d6a41cbd5da53
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2021-06-03 08:36:35 +0900

    beforeDay4

--- src/index.js
+++ src/index.js
@@ -1,38 +1,3 @@
 // <⚠️ DONT DELETE THIS ⚠️>
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
-
-/*
-✅ The text of the title should change when the mouse is on top of it.
-✅ The text of the title should change when the mouse is leaves it.
-✅ When the window is resized the title should change.
-✅ On right click the title should also change.
-✅ The colors of the title should come from a color from the colors array.
-✅ DO NOT CHANGE .css, or .html files.
-✅ ALL function handlers should be INSIDE of "superEventHandler"
-*/
-
-const title = document.querySelector("h2");
-const superEventHandler = {
-  resize: function () {
-    title.innerHTML = "You just resized!";
-    title.style.color = colors[2];
-  },
-  hover: function () {
-    title.innerHTML = "The mouse is here!";
-    title.style.color = colors[0];
-  },
-  leave: function () {
-    title.innerHTML = "The mouse is gone!";
-    title.style.color = colors[1];
-  },
-  rightClick: function () {
-    title.innerHTML = "That was a right click!";
-    title.style.color = colors[4];
-  },
-};
-
-window.addEventListener("resize", superEventHandler.resize);
-title.addEventListener("pointerenter", superEventHandler.hover);
-title.addEventListener("pointerleave", superEventHandler.leave);
-window.addEventListener("contextmenu", superEventHandler.rightClick);
