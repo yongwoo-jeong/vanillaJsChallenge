2d5bf7886d7fa0a6bd59ca712f5a7f36ecd7969d
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2021-06-03 20:58:20 +0900

    doneDay4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 // <⚠️ DONT DELETE THIS ⚠️>
-import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
+
+const h1 = document.querySelector("h1");
+
+function handleResize() {
+  const width = window.outerWidth;
+  if (width > 1200) {
+    h1.style.backgroundColor = "orange";
+  } else if (850 <= width && width <= 1200) {
+    h1.style.backgroundColor = "darkviolet";
+  } else {
+    h1.style.backgroundColor = "mediumblue";
+  }
+}
+
+window.addEventListener("resize", handleResize);
