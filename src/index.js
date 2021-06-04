5e05a1fa42a45f583378945de183cf64c3e06359
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2021-06-04 13:55:14 +0900

    DoneDay5

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@
 function getTime() {
   // Don't delete this.
   const xmasDay = new Date("2021-12-24:00:00:00+0900");
+  const date = new Date();
+  const gap = xmasDay - date;
+  const day = Math.ceil(gap / (1000 * 60 * 60 * 24));
+  const hours = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.ceil((gap % (1000 * 60)) / 1000);
+  timerTitle.innerText = `${day}d ${hours < 10 ? `0${hours}` : hours}h ${
+    minutes < 10 ? `0${minutes}` : minutes
+  }m ${seconds < 10 ? `0${seconds}` : seconds}s`;
 }
 
-function init() {}
+const timerContainer = document.querySelector(".js-timer"),
+  timerTitle = timerContainer.querySelector("h2");
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
 init();
