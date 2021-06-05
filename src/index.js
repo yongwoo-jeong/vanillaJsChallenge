2eb8a17cbbd61d09726a499e62f1217b7d5f098e
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2021-06-06 03:13:37 +0900

    doneDay6

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const selectCountry = document.querySelector("select");
 
+const COUNTRY_LS = "currentCountry";
+
 function valueSubmit() {
   const currentValue = selectCountry.options[selectCountry.selectedIndex].value;
   localStorage.setItem("country", currentValue);
-  console.log(currentValue);
+}
+
+function saveCountry() {
+  const savedCountry = localStorage.getItem("country");
+  selectCountry.value = savedCountry;
 }
 
 function init() {
   selectCountry.addEventListener("change", valueSubmit);
+  saveCountry();
 }
 
 init();
