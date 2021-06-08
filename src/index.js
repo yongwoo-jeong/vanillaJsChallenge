74f71ce0878d2469c8bb84388bdcee11714bb03e
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2021-06-08 15:44:05 +0900

    4.6 doingBtn

--- src/index.js
+++ src/index.js
@@ -1,20 +1,61 @@
-const selectCountry = document.querySelector("select");
+const tasksForm = document.querySelector(".tasksForm"),
+  tasksInput = tasksForm.querySelector("input"),
+  taskList = document.querySelector(".pending");
 
-const COUNTRY_LS = "currentCountry";
+const PENDING_LS = "PENDING";
 
-function valueSubmit() {
-  const currentValue = selectCountry.options[selectCountry.selectedIndex].value;
-  localStorage.setItem("country", currentValue);
+const taskArray = [];
+
+function deleteTask(event) {}
+
+function saveTasks() {
+  localStorage.setItem(PENDING_LS, JSON.stringify(taskArray));
 }
 
-function saveCountry() {
-  const savedCountry = localStorage.getItem("country");
-  selectCountry.value = savedCountry;
+function paintTasks(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "✖";
+  delBtn.addEventListener("click", deleteTask);
+  const doneBtn = document.createElement("button");
+  doneBtn.innerText = "✔";
+  const span = document.createElement("span");
+  const newId = taskArray.length + 1;
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.appendChild(doneBtn);
+  li.id = newId;
+  taskList.appendChild(li);
+  const taskObj = {
+    text: text,
+    id: newId,
+  };
+  taskArray.push(taskObj);
+  saveTasks();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = tasksInput.value;
+  paintTasks(currentValue);
+  tasksInput.value = "";
+}
+
+function loadTasks() {
+  const loadedTasks = localStorage.getItem(PENDING_LS);
+  if (loadedTasks !== null) {
+    const parsedTasks = JSON.parse(loadedTasks);
+    parsedTasks.forEach(function (task) {
+      paintTasks(task.text);
+    });
+  } else {
+  }
 }
 
 function init() {
-  selectCountry.addEventListener("change", valueSubmit);
-  saveCountry();
+  loadTasks();
+  tasksForm.addEventListener("submit", handleSubmit);
 }
 
 init();
